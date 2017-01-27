7908a4e790545167814950e7bc191a0f680e1d54
Lars Langhans <lars.langhans@hypoport.de>
2017-01-27 10:00:46 +0100

    Interface for ObjektMapper erweitert

--- mapper/src/main/java/org/lla_private/rest/json/mapper/IObjectMapperService.java
+++ mapper/src/main/java/org/lla_private/rest/json/mapper/IObjectMapperService.java
@@ -2,5 +2,18 @@ package org.lla_private.rest.json.mapper;
 
 
 public interface IObjectMapperService {
-	String createJsonString(final Object list);
+	/**
+	 * Create a String in JSON format out of the given object
+	 * @param list
+	 * @return
+	 */
+	String createJsonString(final Object aObject);
+
+	/**
+	 * Create an object of resultClass type with content of the json.
+	 * @param json formatted string
+	 * @param resultClass
+	 * @return resultClass type object with json content or NULL
+	 */
+	<T> Object createObject(final String json, final Class<T> resultClass);
 }
